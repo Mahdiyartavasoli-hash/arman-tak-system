3eb85af101765cf899336ce5f981023eefe5a9e5
mahdiyar tavasoli <tavasolimahdy@gmail.com>
2026-07-02 11:02:20 +0330

    feat: implement Web API endpoints for factory analytics using FastAPI (Session 1)
    - Initialized FastAPI server instance inside server.py.
    - Created root route ("/") with a custom JSON welcome message for ARMAN TAK factory.
    - Implemented "/analytics" endpoint to expose total and average production logs via web.
    - Developed "/max_min" endpoint to fetch extreme production records dynamically.
    - Built "/production_count" route utilizing clean-code principles and proper type hinting.

---
+++ server.py
@@ -0,0 +1,32 @@
+
+from fastapi import FastAPI
+import factory_managers
+
+server = FastAPI()
+@server.get("/")
+def home():
+    return {"message": "Welcome to ARMAN TAK Factory API"}
+
+@server.get("/analytics")
+def get_factory_analytics(machine_name: str):
+    analytics = factory_managers.get_machine_analytics(machine_name)
+    return{
+        "machine": machine_name,
+        "total_production": analytics[0],
+        "average_production": analytics[1]
+    }
+@server.get("/max_min")
+def get_factory_max_min(machine_name: str):
+    max_min = factory_managers.get_machine_extremes(machine_name)
+    return{
+        "machine": machine_name,
+        "max_production": max_min[0],
+        "min_production": max_min[1]
+    }
+@server.get("/production_count")
+def get_production_count(machine_name: str):
+    total_runs = factory_managers.get_machine_production_count(machine_name)
+    return{
+        "machine": machine_name,
+        "Total_Production_Count": total_runs,
+    }
