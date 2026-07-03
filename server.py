ccf7e66e57f1ec30fafd8f2c47cf95c24ac25032
mahdiyar tavasoli <tavasolimahdy@gmail.com>
2026-07-03 16:34:47 +0330

    feat: implement POST endpoint for inserting factory production records (Session 2)
    - Integrated Pydantic BaseModel to define the 'ProductionRecord' data schema.
    - Created a POST route ("/insert_production") to handle incoming web data payloads.
    - Linked the web endpoint to the factory database manager for secure SQL data insertion.
    - Successfully verified full database persistence and API connectivity using Swagger UI.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 
 from fastapi import FastAPI
 import factory_managers
+from pydantic import BaseModel
+
 
 server = FastAPI()
 @server.get("/")
@@ -15,6 +17,7 @@ def get_factory_analytics(machine_name: str):
         "total_production": analytics[0],
         "average_production": analytics[1]
     }
+
 @server.get("/max_min")
 def get_factory_max_min(machine_name: str):
     max_min = factory_managers.get_machine_extremes(machine_name)
@@ -23,6 +26,7 @@ def get_factory_max_min(machine_name: str):
         "max_production": max_min[0],
         "min_production": max_min[1]
     }
+
 @server.get("/production_count")
 def get_production_count(machine_name: str):
     total_runs = factory_managers.get_machine_production_count(machine_name)
@@ -30,3 +34,12 @@ def get_production_count(machine_name: str):
         "machine": machine_name,
         "Total_Production_Count": total_runs,
     }
+
+class ProductionRecord(BaseModel):
+    machine_name: str
+    production_amount: float
+@server.post("/insert_production")
+def add_production_log(record: ProductionRecord):
+    factory_managers.save_to_database(record.machine_name, record.production_amount)
+    
+    return {"status": "success", "message": "Record added successfully"}
